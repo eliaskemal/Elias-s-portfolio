74a4ab745bff51f0adc7dfc9e271172a92b943df
eliaskemal <168363911+eliaskemal@users.noreply.github.com>
2026-07-13 15:22:47 +0300

    Fix Express 5 wildcard routing error

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,7 @@ app.post('/api/contact', async (req, res) => {
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get('/(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
